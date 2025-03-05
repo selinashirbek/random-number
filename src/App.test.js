54ac64aaa7ad7a1ccc44725ec5febaaaffcbca4d
selinashirbek <asirbekselin@gmail.com>
2025-03-05 17:50:50 +0500

    Add basic test

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders app without crashing', () => {
+  render(<App />);
+  expect(screen.getByRole('button')).toBeInTheDocument();
+});
